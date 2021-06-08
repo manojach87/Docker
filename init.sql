7b1ea3b7383db4702a0f8a8eb090bbc61b8d4348
manojach87 <manoj.ach@gmail.com>
2021-06-08 00:01:04 -0400

    init.sql

--- init.sql
+++ init.sql
@@ -4,3 +4,4 @@ grant all on hue.* to 'root'@'172.27.1.13' identified by 'secret';
 flush privileges; 
 use hue;
 CREATE TABLE MyGuests (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,firstname VARCHAR(30) NOT NULL,lastname VARCHAR(30) NOT NULL,email VARCHAR(50));
+
